d24b4868656289bc2e6e2a4be9d9badbdd197dba
Subashree-selvaraj <subashreevjc@gmail.com>
2026-06-22 23:47:03 +0530

    feat(shuffle): add Fisher-Yates and naive shuffle implementations with READMEs

---
+++ ARRAYS/FISHER YATES/FisherYatesShuffle.java
@@ -0,0 +1,48 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class FisherYatesShuffle {
+
+    public static void shuffle(int[] arr) {
+        Random random = new Random();
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int value : arr) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Original array:");
+        printArray(arr);
+
+        shuffle(arr);
+
+        System.out.println("Shuffled array:");
+        printArray(arr);
+
+        sc.close();
+    }
+}
