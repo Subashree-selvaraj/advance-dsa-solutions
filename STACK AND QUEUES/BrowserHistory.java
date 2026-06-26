56d1e7bac52a831e8f2b4d5b9a939333b6466b68
Subashree S <subashreevjc@gmail.com>
2026-06-26 10:11:24 +0530

    Create BrowserHistory.java

---
+++ STACK AND QUEUES/BrowserHistory.java
@@ -0,0 +1,39 @@
+class BrowserHistory {
+    private static class Node {
+        String url;
+        Node prev, next;
+
+        Node(String url) {
+            this.url = url;
+        }
+    }
+
+    private Node current;
+
+    public BrowserHistory(String homepage) {
+        current = new Node(homepage);
+    }
+
+    public void visit(String url) {
+        Node node = new Node(url);
+        current.next = node;
+        node.prev = current;
+        current = node;
+    }
+
+    public String back(int steps) {
+        while (steps > 0 && current.prev != null) {
+            current = current.prev;
+            steps--;
+        }
+        return current.url;
+    }
+
+    public String forward(int steps) {
+        while (steps > 0 && current.next != null) {
+            current = current.next;
+            steps--;
+        }
+        return current.url;
+    }
+}
